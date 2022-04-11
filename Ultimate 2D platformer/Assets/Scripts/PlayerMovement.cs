95a7cf8f60788de9dea41a0f46548262240a526b
Juggaluggg <71343792+Juggaluggg@users.noreply.github.com>
2022-04-11 18:25:54 +0100

    Improved ground checking and added animations
    Improved ground checking to stop double jump and added animation for idle and other states for character.

--- Ultimate 2D platformer/Assets/Scripts/PlayerMovement.cs
+++ Ultimate 2D platformer/Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,23 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
-
+    private BoxCollider2D coll;
 
     private float dirX = 0;
+
    [SerializeField] private float moveSpeed = 7f;
    [SerializeField] private float jumpForce = 14f;
-
+   [SerializeField] private LayerMask jumpableGround;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
-
+    private enum MovementState { idle, Running,Jumping, Falling}
+    
     private void Update()
     {
         // movement across X & Y axis 
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
 
 
         //Jumping 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -39,19 +42,37 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState State;
+
         if (dirX > 0f)
         {
-            anim.SetBool("Running", true);
+            State = MovementState.Running;
             sprite.flipX = false;
         }
         else if (dirX < 0F)
         {
-            anim.SetBool("Running", true);
+            State = MovementState.Running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("Running", false);
+            State = MovementState.idle;
+
         }
+
+        if(rb.velocity.y > .1f)
+        {
+            State = MovementState.Jumping;
+        }
+        else if(rb.velocity.y < -.1f)
+        {
+            State = MovementState.Falling;
+        }
+
+        anim.SetInteger("State", (int)State);
+    }
+    private bool IsGrounded()
+    {
+     return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
